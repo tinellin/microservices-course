4be41f4044e87efd0c5616ff181488c5def97a97
tinellin <enzo.tinelli@unesp.br>
2024-02-27 11:18:58 -0300

    add: microservice initial structure: main api

---
+++ main-api/src/main/java/com/microservices/course/main/MainApiApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.course.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApiApplication.class, args);
+	}
+
+}
---
+++ main-api/src/test/java/com/microservices/course/main/MainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.course.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
